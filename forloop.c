3b1d08e610dd0462a63c36ce7698b53cd8fcdbef
mrinali <mrinalikmr@gmail.com>
2026-09-22 00:01:38 +0530

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // type reverse number
+    int n= 13;
+    for ( int i = n; i; i--)  // for( intiallize; test; increment or ddecrement)
+    { printf("%d\n", i) ;
+    }
+    
+    return 0;
+}
